b9cebaddd4958dae21396da442570b87079da810
Nekoyama141592 <mtaky0904@gmail.com>
2023-02-10 16:44:41 +0900

    getxでUIの変更を検知する

--- lib/views/components/product_list.dart
+++ lib/views/components/product_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:great_talk/common/widgets.dart';
 import 'package:great_talk/api/in_app_purchase_api.dart';
 import 'package:great_talk/common/colors.dart';
@@ -18,20 +19,20 @@ class ProductList extends StatelessWidget {
     if (loading) return const Card(child: ListTile(leading: CircularProgressIndicator(),title: Text('情報を取得しています')));
     if (!isAvailable) return const Card();
     final ListTile productHeader = ListTile(title: boldText("プラン一覧"));
-    final List<ListTile> productList = <ListTile>[];
+    final List<Widget> productList = <Widget>[];
     productList.addAll(products.map(
       (ProductDetails productDetails) {
-        return ListTile(
+        return Obx((() => ListTile(
           contentPadding: const EdgeInsets.symmetric(vertical: 8.0,horizontal: 16.0),
           title: productDetails.id == kMonthSubscriptionId ? boldSecondaryColorText("${productDetails.title} おすすめ！") : boldText(productDetails.title),
           trailing: PurchasesController.to.hasProductBeenPurchased(productDetails)
-              ? IconButton(onPressed: () => InAppPurchaseApi.confirmPriceChange(context,inAppPurchase),icon: const Icon(Icons.upgrade))
-              : ElevatedButton(
-                  style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(kSecondaryColor)),
-                  onPressed: () => InAppPurchaseApi.onPurchaseButtonPressed(inAppPurchase, productDetails, purchases),
-                  child: Text(productDetails.price),
-                ),
-        );
+          ? IconButton(onPressed: () => InAppPurchaseApi.confirmPriceChange(context,inAppPurchase),icon: const Icon(Icons.upgrade))
+          : ElevatedButton(
+            style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(kSecondaryColor)),
+            onPressed: () => InAppPurchaseApi.onPurchaseButtonPressed(inAppPurchase, productDetails, purchases),
+            child: Text(productDetails.price),
+          )
+        )));
       },
     ));
 
