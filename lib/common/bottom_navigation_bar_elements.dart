46f0cd70a0fbe170740445b8a1e0c7965ca9af63
Nekoyama141592 <mtaky0904@gmail.com>
2023-02-10 21:49:20 +0900

    bottom icon color

--- lib/common/bottom_navigation_bar_elements.dart
+++ lib/common/bottom_navigation_bar_elements.dart
@@ -1,14 +1,15 @@
 // material
 import 'package:flutter/material.dart';
+import 'package:great_talk/common/colors.dart';
 
 final List<BottomNavigationBarItem> bnbElements = [
   const BottomNavigationBarItem(
     label: 'Chat',
-    icon: Icon(Icons.chat)
+    icon: Icon(Icons.chat,color: kSecondaryColor,)
   ),
   const BottomNavigationBarItem(
     label: 'Plans',
-    icon: Icon(Icons.subscriptions)
+    icon: Icon(Icons.subscriptions,color: kSecondaryColor,)
   ),
 ];
 
