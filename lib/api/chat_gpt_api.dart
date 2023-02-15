8611c594a058be417b47dbd82969c98a86944f3d
Nekoyama141592 <mtaky0904@gmail.com>
2023-02-15 22:29:23 +0900

    max tokens

--- lib/api/chat_gpt_api.dart
+++ lib/api/chat_gpt_api.dart
@@ -3,10 +3,12 @@ import 'package:chat_gpt_flutter/chat_gpt_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ChatGPTApi {
+  static const maxTokens = 1000;
   static Future<String> fetchApi(String prompt) async {
     final chatGpt = ChatGpt(apiKey: dotenv.get("OPEN_AI_API_KEY")); 
     final request = CompletionRequest(
       prompt: prompt,
+      maxTokens: maxTokens,
       model: ChatGptModel.textDavinci003.key,
     );
     return await chatGpt.createCompletion(request) ?? "返答が取得できませんでした";
