6c1d24b7c087020978a37215461be136416d0e52
Nekoyama141592 <mtaky0904@gmail.com>
2023-05-25 01:23:29 +0900

    unix時間

--- lib/extensions/purchase_details_extension.dart
+++ lib/extensions/purchase_details_extension.dart
@@ -1,8 +1,9 @@
 import 'package:in_app_purchase/in_app_purchase.dart';
-
+import 'package:intl/intl.dart';
 extension PurchaseDetailsExtension on PurchaseDetails {
   Map<String, dynamic> toJson() {
     return {
+      "formattedDate": convertUnixTimeToDate(),
       "productID": productID,
       "purchaseID": purchaseID,
       "verificationData": {
@@ -14,5 +15,14 @@ extension PurchaseDetailsExtension on PurchaseDetails {
       "status": status.name,
       "pendingCompletePurchase": pendingCompletePurchase,
     };
+  }
+  String convertUnixTimeToDate() {
+    if (transactionDate == null) {
+      return "";
+    }
+    final x = int.parse(transactionDate!);
+    final date = DateTime.fromMillisecondsSinceEpoch(x);
+    final formattedDate = DateFormat('yyyy-MM-dd').format(date);
+    return formattedDate;
   }
 }
