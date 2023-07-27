ab715ce6efaef92c8ee0bd2d1692d30d964997d8
Nekoyama141592 <mtaky0904@gmail.com>
2023-07-27 20:45:46 +0900

    書き方をシンプルに

--- lib/infrastructure/firestore/firestore_client.dart
+++ lib/infrastructure/firestore/firestore_client.dart
@@ -2,29 +2,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:great_talk/typedefs/firestore_typedef.dart';
 
 class FirestoreClient {
-  FutureQSnapshot getUsers() async {
-    final users = await FirebaseFirestore.instance.collection('users').get();
-    return users;
-  }
 
-  FutureQSnapshot getUsersByFollowerCount() async {
-    final users = await FirebaseFirestore.instance
+  FutureQSnapshot getUsers() async => await FirebaseFirestore.instance.collection('users').get();
+
+  FutureQSnapshot getUsersByFollowerCount() async => await FirebaseFirestore.instance
         .collection('users')
         .orderBy('followerCount', descending: true)
         .get();
-    return users;
-  }
 
-  FutureQSnapshot getPosts() async {
-    final res = await FirebaseFirestore.instance.collectionGroup('posts').get();
-    return res;
-  }
+  FutureQSnapshot getPosts() async => await FirebaseFirestore.instance.collectionGroup('posts').get();
 
-  FutureQSnapshot getPostsgetUsersByLikeCount() async {
-    final res = await FirebaseFirestore.instance
+  FutureQSnapshot getPostsgetUsersByLikeCount() async => await FirebaseFirestore.instance
         .collectionGroup('posts')
         .orderBy('likeCount', descending: true)
         .get();
-    return res;
-  }
 }
