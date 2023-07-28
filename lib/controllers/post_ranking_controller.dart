d08ebec0df9ede2e840d72d928bf2c2ad36998d9
Nekoyama141592 <mtaky0904@gmail.com>
2023-07-28 18:47:13 +0900

    いいね順に投稿を取得するControllerを作成

---
+++ lib/controllers/post_ranking_controller.dart
@@ -0,0 +1,26 @@
+import 'package:great_talk/common/ui_helper.dart';
+import 'package:great_talk/controllers/docs_controller.dart';
+import 'package:great_talk/repository/firestore_repository.dart';
+
+class PostRankingController extends DocsController {
+  final _repository = FirestoreRepository();
+  @override
+  Future<void> fetchDocs() async {
+    final result = await _repository.getPostsByLikeCount();
+    result.when(success: (res) {
+      docs(res);
+    }, failure: () {
+      UIHelper.showFlutterToast("データの取得に失敗しました");
+    });
+  }
+
+  @override
+  Future<void> onLoading() async {
+    final result = await _repository.getMorePostsByLikeCount(docs.last);
+    result.when(success: (res) {
+      addAllDocs(res);
+    }, failure: () {
+      UIHelper.showFlutterToast("データの取得に失敗しました");
+    });
+  }
+}
