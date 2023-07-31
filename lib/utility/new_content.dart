cf80ec127b441fe5d0bae0b738c79e0a9f7db710
Nekoyama141592 <mtaky0904@gmail.com>
2023-07-31 21:18:55 +0900

    現在のユーザーのモックを作成

--- lib/utility/new_content.dart
+++ lib/utility/new_content.dart
@@ -1,14 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:great_talk/infrastructure/firestore/firestore_queries.dart';
+import 'package:great_talk/model/detected_image/detected_image.dart';
+import 'package:great_talk/model/detected_text/detected_text.dart';
 import 'package:great_talk/model/firestore_user/firestore_user.dart';
 
 class NewContent {
+  static DetectedText newDetectedText(String value) => DetectedText(
+      languageCode: "",
+      negativeScore: 0.0,
+      positiveScore: 0.0,
+      sentiment: "",
+      value: value);
+  static DetectedImage newDetectedImage(String url) =>
+      DetectedImage(moderationLabels: [], moderationModelVersion: "", url: url);
   static FirestoreUser newUser(String uid) {
     final now = Timestamp.now();
     return FirestoreUser(
       accountName: "",
       createdAt: now,
-      bio: {},
+      bio: newDetectedText("").toJson(),
       blockCount: 0,
       ethAddress: '',
       followerCount: 0,
@@ -27,8 +37,8 @@ class NewContent {
       searchToken: {},
       uid: uid,
       updatedAt: now,
-      userImage: {},
-      userName: {},
+      userImage: newDetectedImage('').toJson(),
+      userName: newDetectedText('Unknown').toJson(),
       walletAddresses: [],
     );
   }
