aadc7eab4f7d5014ccc3111a15c2d2cdff621779
Nekoyama141592 <mtaky0904@gmail.com>
2023-08-01 11:12:55 +0900

    細かい修正

--- lib/views/main/ranking/components/search_users_screen.dart
+++ lib/views/main/ranking/components/search_users_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:great_talk/controllers/search_users_controller.dart';
-import 'package:great_talk/model/post/post.dart';
+import 'package:great_talk/model/firestore_user/firestore_user.dart';
 import 'package:great_talk/views/screen/refresh_screen.dart';
 
 class SearchUsersScreen extends StatelessWidget {
@@ -14,8 +14,8 @@ class SearchUsersScreen extends StatelessWidget {
         child: ListView.builder(
             itemCount: controller.docs.length,
             itemBuilder: (c, i) {
-              final post = Post.fromJson(controller.docs[i].data());
-              return Text(post.postId);
+              final user = FirestoreUser.fromJson(controller.docs[i].data());
+              return Text(user.typedUserName().value);
             })));
   }
 }
