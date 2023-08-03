fc28b0a3c543a0ba65d0a8b07c3195b14d04c4ee
Nekoyama141592 <mtaky0904@gmail.com>
2023-08-03 13:51:47 +0900

    typed

--- lib/model/custom_complete_text/custom_complete_text.dart
+++ lib/model/custom_complete_text/custom_complete_text.dart
@@ -1,10 +1,12 @@
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:great_talk/typedefs/firestore_typedef.dart';
 part 'custom_complete_text.freezed.dart';
 part 'custom_complete_text.g.dart';
 
 @freezed
-abstract class CustomCompleteText with _$CustomCompleteText {
+abstract class CustomCompleteText implements _$CustomCompleteText {
+  const CustomCompleteText._();
   const factory CustomCompleteText({
     required String systemPrompt,
     double? temperature,
@@ -20,4 +22,12 @@ abstract class CustomCompleteText with _$CustomCompleteText {
   }) = _CustomCompleteText;
   factory CustomCompleteText.fromJson(SDMap json) =>
       _$CustomCompleteTextFromJson(json);
+  FunctionCall? typedFunctionCall() =>
+      functionCall != null ? FunctionCall.values.byName(functionCall!) : null;
+  List<FunctionData>? typedFunctions() => functions
+      ?.map((e) => FunctionData(
+          name: e['name'],
+          description: e['description'],
+          parameters: e['parameters']))
+      .toList();
 }
