eefc0759f2408a2a26a8871a5e1d716490a1974e
Nekoyama141592 <mtaky0904@gmail.com>
2023-08-09 17:29:54 +0900

    Postのデザイン修正

--- lib/views/screen/refresh_screen/refresh_screen.dart
+++ lib/views/screen/refresh_screen/refresh_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
+import 'package:great_talk/common/doubles.dart';
 import 'package:great_talk/controllers/current_user_controller.dart';
 import 'package:great_talk/controllers/docs_controller.dart';
 import 'package:great_talk/model/chat_content/chat_content.dart';
 import 'package:great_talk/model/post/post.dart';
 import 'package:great_talk/views/components/circle_image.dart';
+import 'package:great_talk/views/screen/refresh_screen/components/post_like_button.dart';
+import 'package:great_talk/views/screen/refresh_screen/components/post_report_button.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class RefreshScreen extends HookWidget {
@@ -40,30 +43,47 @@ class RefreshScreen extends HookWidget {
                     itemCount: docsController.docs.length,
                     gridDelegate:
                         const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3, childAspectRatio: 0.7),
+                            crossAxisCount: 3, childAspectRatio: 0.6),
                     itemBuilder: (c, i) {
                       final post = Post.fromJson(docsController.docs[i].data());
                       final content = ChatContent.fromPost(post);
-                      return Obx(() =>
-                          CurrentUserController.to.isValidPost(post.postId)
-                              ? Column(
-                                  children: [
-                                    CircleImage(
-                                      imageValue: content.imageValue,
-                                      onTap: () => Get.toNamed(
-                                          '/chat/users/${post.typedPoster().uid}/posts/${post.postId}'),
+                      return Obx(() => CurrentUserController.to
+                              .isValidPost(post.postId)
+                          ? Padding(
+                              padding: EdgeInsets.all(defaultPadding(context)),
+                              child: Column(
+                                children: [
+                                  CircleImage(
+                                    imageValue: content.imageValue,
+                                    onTap: () => Get.toNamed(
+                                        '/chat/users/${post.typedPoster().uid}/posts/${post.postId}'),
+                                  ),
+                                  Text(
+                                    post.typedTitle().value,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  Text(
+                                    "by ${post.typedPoster().typedUserName().value}",
+                                    style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary),
+                                  ),
+                                  Center(
+                                    child: Row(
+                                      children: [
+                                        PostLikeButton(
+                                          post: post,
+                                        ),
+                                        const Spacer(),
+                                        PostReportButton(post: post)
+                                      ],
                                     ),
-                                    Text(post.typedTitle().value),
-                                    Text(
-                                      "by ${post.typedPoster().typedUserName().value}",
-                                      style: TextStyle(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .secondary),
-                                    ),
-                                  ],
-                                )
-                              : const Text("不適切"));
+                                  )
+                                ],
+                              ),
+                            )
+                          : const Text("不適切"));
                     })));
   }
 }
