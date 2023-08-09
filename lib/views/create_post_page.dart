1e4c49aea0d5b6e93d85229b0590459cee6d9213
Nekoyama141592 <mtaky0904@gmail.com>
2023-08-09 11:00:09 +0900

    細かい調整

--- lib/views/create_post_page.dart
+++ lib/views/create_post_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 import 'package:great_talk/controllers/create_post_controller.dart';
-import 'package:great_talk/views/components/basic_height_box.dart';
 
 class CreatePostPage extends HookWidget {
   const CreatePostPage({Key? key}) : super(key: key);
@@ -15,16 +14,14 @@ class CreatePostPage extends HookWidget {
         ),
         body: Column(
           children: [
-            InkWell(
-              onTap: controller.onImagePickButtonPressed,
-              child: const Icon(
-                Icons.image,
-                size: 100.0,
-              ),
-            ),
-            const BasicHeightBox(),
             Obx(() => controller.uint8List.value == null
-                ? const CircularProgressIndicator()
+                ? InkWell(
+                    onTap: controller.onImagePickButtonPressed,
+                    child: const Icon(
+                      Icons.image,
+                      size: 100.0,
+                    ),
+                  )
                 : Align(
                     alignment: Alignment.center,
                     child: Image.memory(controller.uint8List.value!),
