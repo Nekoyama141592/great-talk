d9394f9e115af513ba572acc10f41fc43b2d33f8
Nekoyama141592 <mtaky0904@gmail.com>
2023-08-12 02:36:53 +0900

    Reloadを実装

--- lib/views/screen/refresh_screen/refresh_screen.dart
+++ lib/views/screen/refresh_screen/refresh_screen.dart
@@ -27,22 +27,35 @@ class RefreshScreen extends HookWidget {
     }, []);
     return Obx(() => docsController.cannotShow()
         ? const LoadingScreen()
-        : SmartRefresher(
-            controller: refreshController,
-            enablePullDown: false, // trueだとiosもAndroidも反応しなくなる
-            enablePullUp: true,
-            header: const WaterDropHeader(),
-            onLoading: () => docsController.onLoading(refreshController),
-            onRefresh: () => docsController.onRefresh(refreshController),
-            child: child ??
-                GridView.builder(
-                    itemCount: docsController.docs.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3, childAspectRatio: 0.5),
-                    itemBuilder: (c, i) {
-                      final post = Post.fromJson(docsController.docs[i].data());
-                      return PostCard(post: post);
-                    })));
+        : docsController.docs.isEmpty
+            ? InkWell(
+                onTap: docsController.onReload,
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Icon(
+                    Icons.refresh,
+                    size: 100.0,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                ),
+              )
+            : SmartRefresher(
+                controller: refreshController,
+                enablePullDown: false, // trueだとiosもAndroidも反応しなくなる
+                enablePullUp: true,
+                header: const WaterDropHeader(),
+                onLoading: () => docsController.onLoading(refreshController),
+                onRefresh: () => docsController.onRefresh(refreshController),
+                child: child ??
+                    GridView.builder(
+                        itemCount: docsController.docs.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 3, childAspectRatio: 0.5),
+                        itemBuilder: (c, i) {
+                          final post =
+                              Post.fromJson(docsController.docs[i].data());
+                          return PostCard(post: post);
+                        })));
   }
 }
