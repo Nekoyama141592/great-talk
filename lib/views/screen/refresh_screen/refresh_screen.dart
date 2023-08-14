79ca8b56b5b018a2f4f848941380b6b54545da13
Nekoyama141592 <mtaky0904@gmail.com>
2023-08-15 07:41:12 +0900

    コードを見やすく修正

--- lib/views/screen/refresh_screen/refresh_screen.dart
+++ lib/views/screen/refresh_screen/refresh_screen.dart
@@ -25,38 +25,41 @@ class RefreshScreen extends HookWidget {
       });
       return refreshController.dispose;
     }, []);
-    return Obx(() => docsController.cannotShow()
-        ? const LoadingScreen()
-        : docsController.docs.isEmpty
-            ? InkWell(
-                onTap: docsController.onReload,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Icon(
-                    Icons.refresh,
-                    size: 100.0,
-                    color: Theme.of(context).colorScheme.secondary,
-                  ),
-                ),
-              )
-            : SmartRefresher(
-                controller: refreshController,
-                enablePullDown: false, // trueだとiosもAndroidも反応しなくなる
-                enablePullUp: true,
-                header: const WaterDropHeader(),
-                onLoading: () => docsController.onLoading(refreshController),
-                onRefresh: () => docsController.onRefresh(refreshController),
-                child: child ??
-                    GridView.builder(
-                        itemCount: docsController.docs.length,
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 3, childAspectRatio: 0.5),
-                        itemBuilder: (c, i) {
-                          final post =
-                              Post.fromJson(docsController.docs[i].doc.data());
-                          final uint8list = docsController.docs[i].uint8list;
-                          return PostCard(post: post, uint8list: uint8list);
-                        })));
+    return Obx(() {
+      if (docsController.cannotShow()) {
+        return const LoadingScreen();
+      }
+      if (docsController.docs.isEmpty) {
+        return InkWell(
+          onTap: docsController.onReload,
+          child: Align(
+            alignment: Alignment.center,
+            child: Icon(
+              Icons.refresh,
+              size: 100.0,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+        );
+      }
+      return SmartRefresher(
+          controller: refreshController,
+          enablePullDown: false, // trueだとiosもAndroidも反応しなくなる
+          enablePullUp: true,
+          header: const WaterDropHeader(),
+          onLoading: () => docsController.onLoading(refreshController),
+          onRefresh: () => docsController.onRefresh(refreshController),
+          child: child ??
+              GridView.builder(
+                  itemCount: docsController.docs.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3, childAspectRatio: 0.5),
+                  itemBuilder: (c, i) {
+                    final post =
+                        Post.fromJson(docsController.docs[i].doc.data());
+                    final uint8list = docsController.docs[i].uint8list;
+                    return PostCard(post: post, uint8list: uint8list);
+                  }));
+    });
   }
 }
