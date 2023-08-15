b061b4b48945018619458e203eab98015f330c7a
Nekoyama141592 <mtaky0904@gmail.com>
2023-08-15 16:34:22 +0900

    chatLimit戻す

--- lib/common/ints.dart
+++ lib/common/ints.dart
@@ -9,7 +9,7 @@ const int maxRequestLength = 3;
 const int maxToken = 3000;
 
 const int timeInSecForIosWeb = 5; // FlutterToast
-const chatLimitPerDay = 50; // TODO: もどせ
+const chatLimitPerDay = 5;
 
 const int nGramIndex = 2; // 検索に使用
 const int plusOne = 1;
