2bce735ed48681f963c3b54e03992a8c2c6f86a2
Nekoyama141592 <mtaky0904@gmail.com>
2023-08-19 18:32:56 +0900

    不要なメソッドを削除

--- lib/controllers/my_profile_controller.dart
+++ lib/controllers/my_profile_controller.dart
@@ -1,32 +1,14 @@
-import 'dart:io';
 import 'package:get/get.dart';
-import 'package:great_talk/common/ui_helper.dart';
 import 'package:great_talk/controllers/abstract/profile_controller.dart';
 import 'package:great_talk/mixin/current_uid_mixin.dart';
 import 'package:great_talk/model/public_user/public_user.dart';
 import 'package:great_talk/utility/file_utility.dart';
-import 'package:great_talk/utility/new_content.dart';
 
 class MyProfileController extends ProfileController with CurrentUserMixin {
   MyProfileController() : super(true);
   static MyProfileController get to => Get.find<MyProfileController>();
-  String bio = '';
-  String userName = '';
-  final Rx<File?> uploadFile = Rx(null);
-
-  Future<void> createUserUpdateLog(String imageUrl) async {
-    final uid = currentUid();
-    final newUserUpdateLog = NewContent.newUserUpdateLog(
-        bio, userName, uid, imageUrl, currentUserRef());
-    final result =
-        await repository.createUserUpdateLog(uid, newUserUpdateLog.toJson());
-    result.when(success: (_) {
-      UIHelper.showFlutterToast("情報が更新されました");
-    }, failure: () {
-      UIHelper.showErrorFlutterToast("情報が更新できませんでした");
-    });
-  }
-
+  
+  // CurrentUserControllerから起動させる.
   Future<void> updateProfileUserState(PublicUser result) async {
     passiveUser(result);
     final s3Image = await FileUtility.getS3Image(
