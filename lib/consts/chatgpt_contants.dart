4aabed45148d727841419c3238bc39d64e34f976
Nekoyama141592 <mtaky0904@gmail.com>
2023-10-04 11:24:00 +0900

    gpt4の最大保持数を3に変更

--- lib/consts/chatgpt_contants.dart
+++ lib/consts/chatgpt_contants.dart
@@ -2,5 +2,5 @@ class ChatGPTConstants {
   static const gptTurboMaxToken = 3000;
   static const gptTurboMaxRequestLength = 3;
   static const gpt4MaxToken = 6000;
-  static const gpt4MaxRequestLength = 5;
+  static const gpt4MaxRequestLength = 3;
 }
