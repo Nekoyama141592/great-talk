682ce81542ae833d4962a6e2e188b259714e4e27
Nekoyama141592 <mtaky0904@gmail.com>
2023-10-17 10:35:46 +0900

    fix:CreatePostControllerがPutされていないバグを解消

--- lib/views/main/my_home_page.dart
+++ lib/views/main/my_home_page.dart
@@ -5,6 +5,7 @@ import 'package:great_talk/common/others.dart';
 import 'package:get/get.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:great_talk/common/texts.dart';
+import 'package:great_talk/controllers/create_post_controller.dart';
 import 'package:great_talk/controllers/current_user_controller.dart';
 import 'package:great_talk/controllers/my_profile_controller.dart';
 import 'package:great_talk/controllers/notification_controller.dart';
@@ -29,6 +30,7 @@ class MyHomePage extends HookWidget {
     Get.put(PostsController());
     Get.put(RealtimeResController());
     Get.put(MyProfileController());
+    Get.put(CreatePostController());
     final pageIndex = useState(0);
     final PageController pageController = usePageController();
     return Scaffold(
--- lib/views/main/my_profile/my_profile_page.dart
+++ lib/views/main/my_profile/my_profile_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:great_talk/controllers/create_post_controller.dart';
 import 'package:great_talk/controllers/my_profile_controller.dart';
 import 'package:great_talk/views/screen/profile_screen/profile_screen.dart';
 
@@ -9,7 +7,7 @@ class MyProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = MyProfileController.to;
-    Get.put(CreatePostController());
+
     return ProfileScreen(
       controller: controller,
     );
