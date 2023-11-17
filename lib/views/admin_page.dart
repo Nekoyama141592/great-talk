2fef679815c732ddb459985499166e9fabaa66b3
Nekoyama141592 <mtaky0904@gmail.com>
2023-11-17 11:38:52 +0900

    add:アプリバージョンを表示

--- lib/views/admin_page.dart
+++ lib/views/admin_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 import 'package:great_talk/common/doubles.dart';
+import 'package:great_talk/common/ints.dart';
 import 'package:great_talk/controllers/admin_controller.dart';
 import 'package:great_talk/controllers/current_user_controller.dart';
 
@@ -30,6 +31,7 @@ class AdminPage extends HookWidget {
             ? const SizedBox.shrink()
             : ListView(
                 children: [
+                  const SelectableText("アプリバージョン: $appVersion"),
                   Obx(() => SelectableText(
                         "登録ユーザー数 ${controller.userCount}",
                         style: style,
