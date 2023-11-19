e57ab1911d4f9c6e6564cf0e32d6263ae34c857f
Nekoyama141592 <mtaky0904@gmail.com>
2023-11-19 15:11:41 +0900

    change:ScaffoldからBasicPage

--- lib/views/loading_page.dart
+++ lib/views/loading_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:great_talk/views/components/basic_page.dart';
 import 'package:great_talk/views/screen/loading_screen.dart';
 
 class LoadingPage extends StatelessWidget {
   const LoadingPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: LoadingScreen());
+    return const BasicPage(child: LoadingScreen());
   }
 }
