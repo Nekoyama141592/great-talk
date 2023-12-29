a16386f2f3a704cf900b7cd611738822db7a815c
Nekoyama141592 <mtaky0904@gmail.com>
2023-12-29 17:48:58 +0900

    change:バージョンアップ

--- lib/consts/remote_config_constants.dart
+++ lib/consts/remote_config_constants.dart
@@ -1,10 +1,12 @@
 import 'package:great_talk/extensions/remote_config_key_extension.dart';
 
 class RemoteConfigConstants {
-  static const int appVersion = 193;
-  static const freeLimitPerDay = 15; // 一日にできる会話数.
-  static const basicLimitPerDay = 10000; // 一日にできる会話数.
-  static const premiumLimitPerDay = 1000; // 一日にできる会話数.
+  static const int appVersion = 195;
+  // 一日にできる会話数.
+  static const freeLimitPerDay = 15; 
+  static const basicLimitPerDay = 10000;
+  static const premiumLimitPerDay = 1000;
+  // メンテナンスメッセージ
   static const String maintenanceMsg = "メンテナンス中";
   static const String forcedUpdateMsg = "最新バージョンへのアップデートが必要です。";
   // key
