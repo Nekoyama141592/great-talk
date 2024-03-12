631231d9fb2ac27904a6760c124bc4e28f949917
Nekoyama141592 <mtaky0904@gmail.com>
2024-03-12 14:13:08 +0900

    change:青色

--- lib/views/main/new_posts/new_posts_screen.dart
+++ lib/views/main/new_posts/new_posts_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:great_talk/common/colors.dart';
 import 'package:great_talk/controllers/new_posts_controller.dart';
 import 'package:great_talk/views/screen/refresh_screen/refresh_screen.dart';
 
@@ -10,7 +11,9 @@ class NewPostsScreen extends StatelessWidget {
     final controller = Get.put(NewPostsController());
     return Scaffold(
         floatingActionButton: FloatingActionButton(
-            onPressed: controller.onRefresh, child: const Icon(Icons.refresh)),
+            onPressed: controller.onRefresh,
+            backgroundColor: kSecondaryColor,
+            child: const Icon(Icons.refresh)),
         body: RefreshScreen(docsController: controller));
   }
 }
