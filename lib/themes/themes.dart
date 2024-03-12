f34b8c2a85a3f5d1427ef8cf90e88b65a9fb38a0
Nekoyama141592 <mtaky0904@gmail.com>
2024-03-12 14:10:31 +0900

    change:theme

--- lib/themes/themes.dart
+++ lib/themes/themes.dart
@@ -2,18 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:great_talk/common/colors.dart';
 
 ThemeData lightThemeData(BuildContext context) {
-  const whiteStyle = TextStyle(color: Colors.white);
   return ThemeData.light().copyWith(
     primaryColor: kPrimaryColor,
     // メインの色が緑なため、他を白で統一
     appBarTheme: const AppBarTheme(
-        color: kPrimaryColor,
-        titleTextStyle: whiteStyle,
-        toolbarTextStyle: whiteStyle,
-        iconTheme: IconThemeData(color: Colors.white)),
+      color: kContentColorLightTheme,
+    ),
     scaffoldBackgroundColor: Colors.white,
     focusColor: Colors.black,
-    cardColor: kContentColorLightTheme,
+    cardColor: kContentColorLightTheme.withOpacity(0.87),
     colorScheme: const ColorScheme.light().copyWith(
         onBackground: kPrimaryColor,
         primary: kSecondaryColor, // CupertinoActionSheetActionのTextカラー
@@ -23,14 +20,11 @@ ThemeData lightThemeData(BuildContext context) {
 }
 
 ThemeData darkThemeData(BuildContext context) {
-  const whiteStyle = TextStyle(color: Colors.white);
   return ThemeData.dark().copyWith(
     primaryColor: kPrimaryColor,
     appBarTheme: const AppBarTheme(
-        color: kPrimaryColor,
-        titleTextStyle: whiteStyle,
-        toolbarTextStyle: whiteStyle,
-        iconTheme: IconThemeData(color: Colors.white)),
+      color: kContentColorDarkTheme,
+    ),
     scaffoldBackgroundColor: kContentColorDarkTheme.withOpacity(0.87),
     focusColor: Colors.white,
     cardColor: kContentColorDarkTheme,
