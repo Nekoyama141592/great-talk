d5e9d920f13e52795462753591a0f5debaa67884
Nekoyama141592 <mtaky0904@gmail.com>
2024-03-29 15:52:35 +0900

    change:プライバシーポリシーの位置

--- lib/views/main/subscribe/subscribe_screen.dart
+++ lib/views/main/subscribe/subscribe_screen.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:great_talk/common/url_redirector.dart';
 import 'package:great_talk/controllers/purchases_controller.dart';
+import 'package:great_talk/views/main/subscribe/components/policy_buttons.dart';
 import 'package:great_talk/views/main/subscribe/components/price_list.dart';
 
 import 'package:great_talk/views/main/subscribe/components/product_list.dart';
-import 'package:great_talk/views/main/subscribe/components/policy_button.dart';
 import 'package:great_talk/views/main/subscribe/components/restore_button.dart';
 
 class SubscribeScreen extends StatelessWidget {
@@ -14,34 +13,17 @@ class SubscribeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final PurchasesController purchasesController = PurchasesController.to;
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          const ProductList(),
-          const RestoreButton(),
-          SingleChildScrollView(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                const PolicyButton(
-                  label: 'プライバシーポリシー',
-                  onPressed: UrlRedirector.toPrivacyPage,
-                ),
-                const PolicyButton(
-                  label: '利用規約',
-                  onPressed: UrlRedirector.toTosPage,
-                ),
-                if (Platform.isIOS)
-                  const PolicyButton(
-                    label: 'EULA',
-                    onPressed: UrlRedirector.toEULAPage,
-                  ),
-              ],
-            ),
-          ),
-          if (Platform.isAndroid && purchasesController.products.isEmpty)
-            const PriceList(),
-        ],
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const PolicyButtons(),
+            const RestoreButton(),
+            const ProductList(),
+            if (Platform.isAndroid && purchasesController.products.isEmpty)
+              const PriceList(),
+          ],
+        ),
       ),
     );
   }
