8b72eb48abd780552ed1dc131a4c30e571ee4e7b
Nekoyama141592 <mtaky0904@gmail.com>
2024-05-14 21:17:08 +0900

    add:basicとpremiumが同じ場合

--- lib/views/main/subscribe/components/product_list.dart
+++ lib/views/main/subscribe/components/product_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:great_talk/controllers/purchases_controller.dart';
+import 'package:great_talk/controllers/remote_config_controller.dart';
 import 'package:great_talk/iap_constants/subscription_constants.dart';
 import 'package:great_talk/views/main/subscribe/components/plan_description.dart';
 import 'package:great_talk/views/main/subscribe/components/product_card.dart';
@@ -30,13 +31,20 @@ class ProductList extends StatelessWidget {
                       PlanDescription(text: '無制限のチャット'),
                       PlanDescription(text: 'コピー可能な説明や返答'),
                     ])
-              : ProductCard(
-                  productDetails: productDetails,
-                  descriptions: const [
-                      PlanDescription(text: 'ベーシックプランの全ての機能'),
-                      PlanDescription(text: '高精度な応答をするAI'),
-                      PlanDescription(text: '画像を生成するAI'),
-                    ]);
+              : ProductCard(productDetails: productDetails, descriptions: [
+                  const PlanDescription(text: 'ベーシックプランの全ての機能'),
+                  Obx(() {
+                    final remoteConfigController = RemoteConfigController.to;
+                    final basic = remoteConfigController.rxBasicModel.value;
+                    final premium = remoteConfigController.rxPremiumModel.value;
+                    if (basic == premium) {
+                      return const SizedBox.shrink();
+                    } else {
+                      return PlanDescription(text: "モデルが$basicから$premiumに!");
+                    }
+                  }),
+                  const PlanDescription(text: '画像を生成するAI'),
+                ]);
         },
       ).toList();
 
