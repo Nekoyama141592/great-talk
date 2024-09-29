7c4bf106866f04bd3f57eb1b93152a3a6833c4bc
Nekoyama141592 <mtaky0904@gmail.com>
2024-09-30 00:42:01 +0900

    add: 第二世代の関数作る

--- cloud_functions/dev/functions/index.js
+++ cloud_functions/dev/functions/index.js
@@ -1,14 +1,22 @@
+// const admin = require("firebase-admin");
+// admin.initializeApp();
+// const fs = require('fs');
+// const path = require('path');
+
+// const handlersPath = path.join(__dirname, 'src', 'handlers');
+// const handlers = {};
+
+// fs.readdirSync(handlersPath).forEach(file => {
+//     const handlerName = path.basename(file, '.js');
+//     handlers[handlerName] = require(path.join(handlersPath, file))[handlerName];
+// });
+
+// module.exports = handlers;
 const admin = require("firebase-admin");
 admin.initializeApp();
-const fs = require('fs');
-const path = require('path');
 
-const handlersPath = path.join(__dirname, 'src', 'handlers');
-const handlers = {};
+const {generateText} = require('./src/handlers/generateText');
 
-fs.readdirSync(handlersPath).forEach(file => {
-    const handlerName = path.basename(file, '.js');
-    handlers[handlerName] = require(path.join(handlersPath, file))[handlerName];
-});
-
-module.exports = handlers;
+module.exports = {
+    generateText
+};
