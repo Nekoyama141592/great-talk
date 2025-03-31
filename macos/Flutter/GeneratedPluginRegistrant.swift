bcbfd9ac996d1ebe3d9f63392f5c887828d6c157
Nekoyama141592 <mtaky0904@gmail.com>
2025-03-31 17:54:01 +0900

    fix: ios

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -22,7 +22,7 @@ import in_app_review
 import path_provider_foundation
 import shared_preferences_foundation
 import sign_in_with_apple
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
