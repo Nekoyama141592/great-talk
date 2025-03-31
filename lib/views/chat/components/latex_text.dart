7218defe9faa282e59f1fa5758fde518f0324c4b
Nekoyama141592 <mtaky0904@gmail.com>
2025-03-31 17:17:00 +0900

    fix

--- lib/views/chat/components/latex_text.dart
+++ lib/views/chat/components/latex_text.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_markdown_latex/flutter_markdown_latex.dart';
-import 'package:get/get.dart';
-import 'package:great_talk/controllers/purchases_controller.dart';
 import 'package:markdown/markdown.dart' as md;
 
 const text = r'''
