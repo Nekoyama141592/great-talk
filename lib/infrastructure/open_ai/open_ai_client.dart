9529f04d1b1485c0de2a318b8c7fc3456d5a3800
Nekoyama141592 <mtaky0904@gmail.com>
2025-05-13 14:05:40 +0900

    add: retrofit

--- lib/infrastructure/open_ai/open_ai_client.dart
+++ lib/infrastructure/open_ai/open_ai_client.dart
@@ -4,8 +4,7 @@ import 'package:great_talk/model/open_ai/generata_image/generate_image_request/g
 import 'package:great_talk/model/open_ai/generata_image/generate_image_response/generate_image_response.dart';
 import 'package:great_talk/model/open_ai/generate_text/generate_text_request/generate_text_response.dart';
 import 'package:great_talk/model/open_ai/generate_text/generate_text_response/generate_text_request.dart';
-import 'package:retrofit/http.dart';
-
+import 'package:retrofit/retrofit.dart';
 part 'open_ai_client.g.dart';
 
 @RestApi()
