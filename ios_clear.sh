455d62598caaa568131af278fdf9d27bf596990c
Nekoyama141592 <mtaky0904@gmail.com>
2025-05-13 14:00:42 +0900

    update packages

---
+++ ios_clear.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -rf ios/Pods
+
+rm -rf ~/Library/Developer/Xcode/DerivedData/
+
+flutter clean
+
+flutter pub cache repair
+
+rm ios/Podfile.lock
+
+flutter pub get
+
+cd ios
+
+pod repo update
+
+pod install
+
+cd ..
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,9 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_crashlytics
+import firebase_messaging
 import firebase_remote_config
+import firebase_storage
 import flutter_app_badger
 import flutter_image_compress_macos
 import flutter_local_notifications
@@ -27,12 +29,14 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
+  FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterAppBadgerPlugin.register(with: registry.registrar(forPlugin: "FlutterAppBadgerPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
