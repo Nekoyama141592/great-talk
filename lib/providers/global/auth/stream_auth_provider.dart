1b8ee58fc2dc84dee67e3f7219f1bd5d3c4b0401
Nekoyama141592 <mtaky0904@gmail.com>
2025-05-23 14:26:38 +0900

    refactor: authUid

---
+++ lib/providers/global/auth/stream_auth_provider.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'stream_auth_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+Stream<String?> streamAuthUid(Ref ref) {
+  final userStream = FirebaseAuth.instance.authStateChanges();
+  return userStream.map((user) => user?.uid);
+}
