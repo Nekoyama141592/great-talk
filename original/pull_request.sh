f4f0d2acc8e92bb631065a0f5c4945c937f87180
Nekoyama141592 <mtaky0904@gmail.com>
2025-06-01 00:58:42 +0900

    add: pull_request.sh

---
+++ original/pull_request.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Pull Requestを作成するスクリプト.
+# 実行コマンド
+# source original/pull_request.sh
+
+## TODO: 新しいブランチ名を入力
+branch_name=""
+
+git checkout develop
+git pull origin develop
+git checkout -b "${branch_name}"
+git commit -m 'make pull request' --allow-empty
+git push --set-upstream origin "${branch_name}"
