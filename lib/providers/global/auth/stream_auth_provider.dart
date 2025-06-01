eb5513196ddcdaa0a54e4090b3c327af85776aac
Nekoyama141592 <mtaky0904@gmail.com>
2025-06-01 12:20:59 +0900

    add: Providerで取得する

--- lib/providers/global/auth/stream_auth_provider.dart
+++ lib/providers/global/auth/stream_auth_provider.dart
@@ -1,17 +1,18 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:great_talk/providers/client/firebath_auth/firebase_auth_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'stream_auth_provider.g.dart';
 
 @Riverpod(keepAlive: true)
 Stream<String?> streamAuthUid(Ref ref) {
-  final userStream = FirebaseAuth.instance.authStateChanges();
+  final userStream = ref.watch(firebaseAuthProvider).authStateChanges();
   return userStream.map((user) => user?.uid);
 }
 
 @Riverpod(keepAlive: true)
 Stream<User?> streamAuth(Ref ref) {
-  return FirebaseAuth.instance.authStateChanges();
+  return ref.watch(firebaseAuthProvider).authStateChanges();
 }
