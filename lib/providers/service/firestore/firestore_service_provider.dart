506a626e732a229acbb16062afc2b3c4be413571
Nekoyama141592 <mtaky0904@gmail.com>
2025-06-01 16:49:06 +0900

    add: firestoreServiceProvider

---
+++ lib/providers/service/firestore/firestore_service_provider.dart
@@ -0,0 +1,5 @@
+import 'package:great_talk/service/firestore_service.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'firestore_service_provider.g.dart';
+@riverpod
+FirestoreService firestoreService() => FirestoreService();
