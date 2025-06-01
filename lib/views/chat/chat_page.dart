54eb25f1eb46a5d67d33b045bbebddc03af88d43
Nekoyama141592 <mtaky0904@gmail.com>
2025-06-01 15:44:25 +0900

    remove: SafeArea

--- lib/views/chat/chat_page.dart
+++ lib/views/chat/chat_page.dart
@@ -58,183 +58,181 @@ class ChatPage extends HookConsumerWidget {
           messages[index - 1].createdAtDateTime.day;
     }
 
-    return SafeArea(
-      child: Scaffold(
-        // AsyncValue.when を使って、ローディング・エラー・データ表示を切り替える
-        body: chatStateAsync.when(
-          loading: () => const Center(child: CircularProgressIndicator()),
-          error:
-              (err, stack) => Scaffold(
-                appBar: AppBar(),
-                body: Center(child: Text('エラーが発生しました: $stack')),
-              ),
-          data: (ChatState data) {
-            final Post post = data.post;
-            final List<TextMessage> messages = data.messages;
-            final bool isGenerating = data.isGenerating;
-            final String currentUserId =
-                ref.watch(streamAuthUidProvider).value ?? "";
-
-            return Scaffold(
-              appBar: AppBar(
-                title: EllipsisText(post.typedTitle().value),
-                actions: [
-                  // 自分の投稿、もしくは管理者なら削除ボタン、それ以外ならレポートボタンを表示
-                  if (post.uid == currentUserId || isAdmin)
-                    DeletePostButton(
-                      onTap: () async {
-                        ref
-                            .read(tokensNotifierProvider.notifier)
-                            .addDeletePostId(postId); // 楽観的に追加する
-                        final result = await ref
-                            .read(postUsecaseProvider)
-                            .deletePost(post);
-                        result.when(
-                          success: (_) async {
-                            UIHelper.showSuccessSnackBar(context, "投稿を削除しました。");
-                            RouterCore.back(context);
-                          },
-                          failure: (e) {
-                            // 失敗したら元に戻す
-                            ref
-                                .read(tokensNotifierProvider.notifier)
-                                .removeDeletePostId(postId);
-                            UIHelper.showFailureSnackBar(
-                              context,
-                              "投稿を削除することができませんでした。",
-                            );
-                          },
-                        );
-                      },
-                    )
-                  else
-                    AppBarAction(
-                      onTap: () {
-                        final notifier = ref.read(postUsecaseProvider);
-                        PostUiCore.onReportButtonPressed(
-                          context: context,
-                          mutePost: (innerContext) async {
-                            final token = ref
-                                .read(tokensNotifierProvider.notifier)
-                                .addMutePost(post);
-                            if (token == null) {
-                              RouterCore.back(innerContext);
-                              return;
-                            }
-                            final result = await notifier.mutePost(
-                              post,
-                              currentUserId,
-                              token,
-                            );
-                            result.when(
-                              success: (_) => RouterCore.back(innerContext),
-                              failure: (_) {},
-                            );
-                          },
-                          muteUser: (innerContext) async {
-                            final token = ref
-                                .read(tokensNotifierProvider.notifier)
-                                .addMuteUser(post);
-                            if (token == null) {
-                              RouterCore.back(innerContext);
-                              return;
-                            }
-                            final result = await notifier.muteUser(
-                              post,
-                              currentUserId,
-                              token,
-                            );
-                            result.when(
-                              success: (_) => RouterCore.back(innerContext),
-                              failure: (_) {},
-                            );
-                          },
-                        );
-                      },
-                      child: const Icon(Icons.report),
-                    ),
-                  PostLikeButton(isHorizontal: true, post: post),
-                  //
-                  MenuButton(
-                    onMenuPressed: () {
-                      RouterCore.back(context);
-                      ChatUiCore.onMenuPressed(
-                        context: context,
-                        post: post,
-                        cleanLocalMessage: chatNotifier.cleanLocalMessage,
+    return Scaffold(
+      // AsyncValue.when を使って、ローディング・エラー・データ表示を切り替える
+      body: chatStateAsync.when(
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error:
+            (err, stack) => Scaffold(
+              appBar: AppBar(),
+              body: Center(child: Text('エラーが発生しました: $stack')),
+            ),
+        data: (ChatState data) {
+          final Post post = data.post;
+          final List<TextMessage> messages = data.messages;
+          final bool isGenerating = data.isGenerating;
+          final String currentUserId =
+              ref.watch(streamAuthUidProvider).value ?? "";
+    
+          return Scaffold(
+            appBar: AppBar(
+              title: EllipsisText(post.typedTitle().value),
+              actions: [
+                // 自分の投稿、もしくは管理者なら削除ボタン、それ以外ならレポートボタンを表示
+                if (post.uid == currentUserId || isAdmin)
+                  DeletePostButton(
+                    onTap: () async {
+                      ref
+                          .read(tokensNotifierProvider.notifier)
+                          .addDeletePostId(postId); // 楽観的に追加する
+                      final result = await ref
+                          .read(postUsecaseProvider)
+                          .deletePost(post);
+                      result.when(
+                        success: (_) async {
+                          UIHelper.showSuccessSnackBar(context, "投稿を削除しました。");
+                          RouterCore.back(context);
+                        },
+                        failure: (e) {
+                          // 失敗したら元に戻す
+                          ref
+                              .read(tokensNotifierProvider.notifier)
+                              .removeDeletePostId(postId);
+                          UIHelper.showFailureSnackBar(
+                            context,
+                            "投稿を削除することができませんでした。",
+                          );
+                        },
                       );
                     },
+                  )
+                else
+                  AppBarAction(
+                    onTap: () {
+                      final notifier = ref.read(postUsecaseProvider);
+                      PostUiCore.onReportButtonPressed(
+                        context: context,
+                        mutePost: (innerContext) async {
+                          final token = ref
+                              .read(tokensNotifierProvider.notifier)
+                              .addMutePost(post);
+                          if (token == null) {
+                            RouterCore.back(innerContext);
+                            return;
+                          }
+                          final result = await notifier.mutePost(
+                            post,
+                            currentUserId,
+                            token,
+                          );
+                          result.when(
+                            success: (_) => RouterCore.back(innerContext),
+                            failure: (_) {},
+                          );
+                        },
+                        muteUser: (innerContext) async {
+                          final token = ref
+                              .read(tokensNotifierProvider.notifier)
+                              .addMuteUser(post);
+                          if (token == null) {
+                            RouterCore.back(innerContext);
+                            return;
+                          }
+                          final result = await notifier.muteUser(
+                            post,
+                            currentUserId,
+                            token,
+                          );
+                          result.when(
+                            success: (_) => RouterCore.back(innerContext),
+                            failure: (_) {},
+                          );
+                        },
+                      );
+                    },
+                    child: const Icon(Icons.report),
                   ),
+                PostLikeButton(isHorizontal: true, post: post),
+                //
+                MenuButton(
+                  onMenuPressed: () {
+                    RouterCore.back(context);
+                    ChatUiCore.onMenuPressed(
+                      context: context,
+                      post: post,
+                      cleanLocalMessage: chatNotifier.cleanLocalMessage,
+                    );
+                  },
+                ),
+              ],
+            ),
+            body: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const BasicHeightBox(),
+                  SizedBox(
+                    height: chatScreenHeight(context),
+                    child: ListView.builder(
+                      controller: scrollController,
+                      itemCount: messages.length,
+                      itemBuilder: ((context, index) {
+                        // 生成中のメッセージかどうかを判定
+                        final bool isGeneratingMsg =
+                            isGenerating && index == messages.length - 1;
+                        final image = data.postImage;
+                        if (isGeneratingMsg) {
+                          // AIがメッセージを生成中の表示
+                          return MsgCard(
+                            isMyMsg: false,
+                            isAnotherDay: isAnotherDay(messages, index),
+                            text: data.realtimeRes, // リアルタイムで更新されるテキスト
+                            createdAt: messages[index].typedCreatedAt(),
+                            postImage:
+                                image != null ? base64Decode(image) : null,
+                          );
+                        } else {
+                          // 通常のメッセージ表示
+                          final message = messages[index];
+                          final String text = message.typedText().value;
+                          final bool isMyMessage =
+                              message.senderUid == currentUserId;
+    
+                          return MsgCard(
+                            isMyMsg: isMyMessage,
+                            isAnotherDay: isAnotherDay(messages, index),
+                            text: text,
+                            createdAt: message.typedCreatedAt(),
+                            postImage:
+                                image != null ? base64Decode(image) : null,
+                          );
+                        }
+                      }),
+                    ),
+                  ),
+                  // メッセージ生成中でなければ入力フィールドを表示
+                  if (!isGenerating)
+                    Row(
+                      children: [
+                        const BasicWidthBox(),
+                        RoundedInputField(
+                          controller: inputController,
+                          send:
+                              () => chatNotifier.onSendPressed(
+                                FocusScope.of(context).unfocus,
+                                inputController,
+                                scrollController,
+                              ),
+                        ),
+                      ],
+                    )
+                  else
+                    const SizedBox.shrink(), // 生成中は非表示
                 ],
               ),
-              body: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    const BasicHeightBox(),
-                    SizedBox(
-                      height: chatScreenHeight(context),
-                      child: ListView.builder(
-                        controller: scrollController,
-                        itemCount: messages.length,
-                        itemBuilder: ((context, index) {
-                          // 生成中のメッセージかどうかを判定
-                          final bool isGeneratingMsg =
-                              isGenerating && index == messages.length - 1;
-                          final image = data.postImage;
-                          if (isGeneratingMsg) {
-                            // AIがメッセージを生成中の表示
-                            return MsgCard(
-                              isMyMsg: false,
-                              isAnotherDay: isAnotherDay(messages, index),
-                              text: data.realtimeRes, // リアルタイムで更新されるテキスト
-                              createdAt: messages[index].typedCreatedAt(),
-                              postImage:
-                                  image != null ? base64Decode(image) : null,
-                            );
-                          } else {
-                            // 通常のメッセージ表示
-                            final message = messages[index];
-                            final String text = message.typedText().value;
-                            final bool isMyMessage =
-                                message.senderUid == currentUserId;
-
-                            return MsgCard(
-                              isMyMsg: isMyMessage,
-                              isAnotherDay: isAnotherDay(messages, index),
-                              text: text,
-                              createdAt: message.typedCreatedAt(),
-                              postImage:
-                                  image != null ? base64Decode(image) : null,
-                            );
-                          }
-                        }),
-                      ),
-                    ),
-                    // メッセージ生成中でなければ入力フィールドを表示
-                    if (!isGenerating)
-                      Row(
-                        children: [
-                          const BasicWidthBox(),
-                          RoundedInputField(
-                            controller: inputController,
-                            send:
-                                () => chatNotifier.onSendPressed(
-                                  FocusScope.of(context).unfocus,
-                                  inputController,
-                                  scrollController,
-                                ),
-                          ),
-                        ],
-                      )
-                    else
-                      const SizedBox.shrink(), // 生成中は非表示
-                  ],
-                ),
-              ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
