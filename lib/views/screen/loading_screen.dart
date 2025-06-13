553d368d63f7b4d2bf3c38c2fb36c7977aae9f8c
Nekoyama141592 <mtaky0904@gmail.com>
2025-06-13 17:32:02 +0900

    fix: Loadingを真ん中に表示

--- lib/views/screen/loading_screen.dart
+++ lib/views/screen/loading_screen.dart
@@ -5,22 +5,20 @@ class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Align(
-      alignment: Alignment.center,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 100.0,
-            width: double.infinity,
-            child: Align(
-              alignment: Alignment.center,
-              child: CircularProgressIndicator(),
-            ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 100.0,
+          width: double.infinity,
+          child: Align(
+            alignment: Alignment.center,
+            child: CircularProgressIndicator(),
           ),
-          BasicHeightBox(),
-          Text("ロード中..."),
-        ],
-      ),
+        ),
+        BasicHeightBox(),
+        Text("ロード中..."),
+      ],
     );
   }
 }
