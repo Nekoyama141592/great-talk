9ee86042ed246df975caaaa9e60b3e25975f471a
Nekoyama141592 <mtaky0904@gmail.com>
2025-06-22 21:56:47 +0900

    add: JsonCoreのテスト

--- lib/core/json_core.dart
+++ lib/core/json_core.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 class JsonCore {
-  static Map<String, dynamic> encodeDecode(dynamic data) {
+  static Map<String, dynamic> cast(dynamic data) {
     final encoded = json.encode(data);
     final decoded = json.decode(encoded);
     return decoded;
