af5e13cfa4e143fa947f9772bc440f95ced61a5c
Nekoyama141592 <mtaky0904@gmail.com>
2025-06-25 01:17:24 +0900

    add: CreatePostPageを分割

---
+++ lib/views/create_post/components/create_post_form.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:great_talk/consts/form_consts.dart';
+import 'package:great_talk/model/view_model_state/create_post/create_post_state.dart';
+import 'package:great_talk/provider/view_model/create_post/create_post_view_model.dart';
+import 'package:great_talk/views/create_post/components/description_field.dart';
+import 'package:great_talk/views/create_post/components/system_prompt_field.dart';
+import 'package:great_talk/views/create_post/components/title_field.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class CreatePostForm extends ConsumerWidget {
+  const CreatePostForm({
+    super.key,
+    required this.formKey,
+    required this.state,
+    required this.deviceHeight,
+  });
+
+  final GlobalKey<FormState> formKey;
+  final CreatePostState? state;
+  final double deviceHeight;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewModel = ref.read(createPostViewModelProvider.notifier);
+
+    return SizedBox(
+      height: deviceHeight * 0.50,
+      child: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SystemPromptField(
+                initialValue: state?.systemPrompt ?? FormConsts.defaultSystemPrompt,
+                onSaved: viewModel.setSystemPrompt,
+              ),
+              DescriptionField(
+                initialValue: state?.description ?? '',
+                onSaved: viewModel.setDescription,
+              ),
+              TitleField(
+                initialValue: state?.title ?? '',
+                onSaved: viewModel.setTitle,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
