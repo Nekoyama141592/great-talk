5a10dd622794e4b64af66360247d3f23fbd165d0
Nekoyama141592 <mtaky0904@gmail.com>
2025-06-25 23:56:34 +0900

    remove: views

--- lib/views/common/ranking_tab_bar_elements.dart
+++
@@ -1,9 +0,0 @@
-class RankingTabBarElements {
-  RankingTabBarElements(this.title);
-  final String title;
-}
-
-final List<RankingTabBarElements> rankingTBE = [
-  RankingTabBarElements('投稿'),
-  RankingTabBarElements('ユーザー'),
-];
--- lib/views/components/basic_width_box.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class BasicWidthBox extends StatelessWidget {
-  const BasicWidthBox({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const SizedBox(width: 20.0);
-  }
-}
