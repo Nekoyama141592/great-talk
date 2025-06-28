b734e7c5a200457857380789fb6ed7fa22f29276
Nekoyama141592 <mtaky0904@gmail.com>
2025-06-28 21:30:27 +0900

    add: FirebaseFunctionsExceptionを使用

--- lib/infrastructure/repository/api_repository.dart
+++ lib/infrastructure/repository/api_repository.dart
@@ -46,7 +46,7 @@ class ApiRepository implements IApiRepository {
       final result = await _call(name, request.toJson());
       final res = PutObjectResponse.fromJson(result);
       return rs.Result.success(res);
-    } catch (e) {
+    } on FirebaseFunctionsException catch (e) {
       debugPrint('putObject: ${e.toString()}');
       return rs.Result.failure('画像のアップロードが失敗しました');
     }
@@ -60,7 +60,7 @@ class ApiRepository implements IApiRepository {
       final res = GetObjectResponse.fromJson(result);
       final base64Image = res.base64Image;
       return rs.Result.success(base64Image);
-    } catch (e) {
+    } on FirebaseFunctionsException catch (e) {
       debugPrint('getObject: ${e.toString()}');
       return rs.Result.failure('画像の取得が失敗しました');
     }
@@ -77,7 +77,7 @@ class ApiRepository implements IApiRepository {
       final res = DeleteObjectResponse.fromJson(result);
       debugPrint('画像の削除が成功しました');
       return rs.Result.success(res);
-    } catch (e) {
+    } on FirebaseFunctionsException catch (e) {
       debugPrint('deleteObject: ${e.toString()}');
       return rs.Result.failure('画像の削除が失敗しました');
     }
@@ -94,7 +94,7 @@ class ApiRepository implements IApiRepository {
       final result = await _call(name, request.toJson());
       final res = GenerateImageResponse.fromJson(result);
       return rs.Result.success(res);
-    } catch (e) {
+    } on FirebaseFunctionsException catch (e) {
       debugPrint('generateImage: ${e.toString()}');
       return rs.Result.failure('画像の生成に失敗しました');
     }
@@ -110,7 +110,7 @@ class ApiRepository implements IApiRepository {
       final result = await _call(name, requestData);
       final res = GenerateTextResponse.fromJson(result);
       return rs.Result.success(res);
-    } catch (e) {
+    } on FirebaseFunctionsException catch (e) {
       debugPrint('generateText: ${e.toString()}');
       return rs.Result.failure('テキストの生成に失敗しました');
     }
@@ -126,7 +126,7 @@ class ApiRepository implements IApiRepository {
       final result = await _call(name, request.toJson());
       final res = VerifiedPurchase.fromJson(result);
       return rs.Result.success(res);
-    } catch (e) {
+    } on FirebaseFunctionsException catch (e) {
       debugPrint('verifyAndroidReceipt: ${e.toString()}');
       return rs.Result.failure('購入の検証が失敗しました');
     }
@@ -142,7 +142,7 @@ class ApiRepository implements IApiRepository {
       final result = await _call(name, request.toJson());
       final res = VerifiedPurchase.fromJson(result);
       return rs.Result.success(res);
-    } catch (e) {
+    } on FirebaseFunctionsException catch (e) {
       debugPrint('verifyIOSReceipt: ${e.toString()}');
       return rs.Result.failure('購入の検証が失敗しました');
     }
