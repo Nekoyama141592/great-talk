fb9260722fd453c1fe3d878895482437c720b154
Nekoyama141592 <mtaky0904@gmail.com>
2025-06-30 00:18:09 +0900

    change: TODOを上に

--- original/flutter_integration_test.sh
+++ original/flutter_integration_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # テスト完了を通知するシェル
-# 実行コマンド source original/flutter_integration_test.sh
 # TODO: lib/presentation/page/check_page/first_page.dartのVerifyEmailPageをコメントアウトする
+# 実行コマンド source original/flutter_integration_test.sh
 flutter test integration_test
 echo "\a"
