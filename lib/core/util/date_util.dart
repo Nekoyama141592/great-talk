226c52def005d0761b721908b3f81915e4817240
Nekoyama141592 <mtaky0904@gmail.com>
2025-07-03 11:29:54 +0900

    refactor: DateConveterを統合

--- lib/core/util/date_util.dart
+++ lib/core/util/date_util.dart
@@ -21,4 +21,23 @@ class DateUtil {
     final day = dateTime.day.toString().padLeft(2, '0');
     return '$year/$month/$day';
   }
+
+  static DateTime intToDateTime(int date) {
+    var year = date ~/ 10000;
+    var month = (date % 10000) ~/ 100;
+    var day = date % 100;
+    return DateTime(year, month, day);
+  }
+
+  static int dateTimeToInt(DateTime date) {
+    return (date.year * 10000) + (date.month * 100) + date.day;
+  }
+
+  static int nowDateTime() => DateTime.now().millisecondsSinceEpoch;
+
+  static bool isCrossingDate(DateTime startDate, DateTime endDate) {
+    return startDate.day != endDate.day ||
+        startDate.month != endDate.month ||
+        startDate.year != endDate.year;
+  }
 }
