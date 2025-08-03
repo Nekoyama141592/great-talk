3d407ddb2c91f9832d37fde1fc74af71f5a48519
Nekoyama141592 <mtaky0904@gmail.com>
2025-08-03 16:05:32 +0900

    change: price

--- lib/presentation/constant/purchase_constants.dart
+++ lib/presentation/constant/purchase_constants.dart
@@ -1,6 +1,6 @@
 class PurchaseConstants {
-  static const proPrice = '¥980';
-  static const premiumPrice = '¥7980';
+  static const proPrice = '¥600';
+  static const premiumPrice = '¥4000';
   static const proTitle = "プロ";
   static const premiumTitle = "プレミアム";
 }
