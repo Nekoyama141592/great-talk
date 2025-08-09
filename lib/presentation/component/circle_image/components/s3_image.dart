420b861f79d376abdc91c353f74e40a621933754
Nekoyama141592 <mtaky0904@gmail.com>
2025-08-10 02:15:28 +0900

    add: スケルトンローディング

--- lib/presentation/component/circle_image/components/s3_image.dart
+++ lib/presentation/component/circle_image/components/s3_image.dart
@@ -1,14 +1,25 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:great_talk/core/util/size_util.dart';
+import 'package:shimmer/shimmer.dart';
 
 class S3Image extends StatelessWidget {
-  const S3Image({super.key, this.width, this.height, required this.isModerated,required this.imageUrl});
+  const S3Image({
+    super.key,
+    this.width,
+    this.height,
+    required this.isModerated,
+    required this.imageUrl,
+  });
+
   final double? width, height;
   final bool isModerated;
   final String imageUrl;
+
   @override
   Widget build(BuildContext context) {
+    final double imageSize = width ?? SizeUtil.userImageSize(context);
+
     return Align(
       alignment: Alignment.center,
       child: Container(
@@ -18,36 +29,42 @@ class S3Image extends StatelessWidget {
         ),
         child: ClipOval(
           child: SizedBox(
-            width: width ?? SizeUtil.userImageSize(context),
-            height: height ?? SizeUtil.userImageSize(context),
-            child: isModerated ? CachedNetworkImage(
-              imageUrl: imageUrl,
-              fit: BoxFit.cover,
-              width: width ?? SizeUtil.userImageSize(context),
-              height: height ?? SizeUtil.userImageSize(context),
-              placeholder:
-                  (context, url) => Container(
-                    color: Colors.grey[300],
-                    child: Icon(
-                      Icons.person,
-                      size: (width ?? SizeUtil.userImageSize(context)) * 0.6,
-                      color: Colors.grey[600],
+            width: imageSize,
+            height: imageSize,
+            child: isModerated
+                ? CachedNetworkImage(
+                    imageUrl: imageUrl,
+                    fit: BoxFit.cover,
+                    width: imageSize,
+                    height: imageSize,
+                    placeholder: (context, url) =>
+                        Shimmer.fromColors(
+                      baseColor: Colors.grey[300]!,
+                      highlightColor: Colors.grey[100]!,
+                      child: Container(
+                        color: Colors.white,
+                        child: Icon(
+                          Icons.person,
+                          size: imageSize * 0.6,
+                          color: Colors.grey[600],
+                        ),
+                      ),
                     ),
+                    errorWidget: (context, url, error) =>
+                        Container(
+                      color: Colors.grey[300],
+                      child: Icon(
+                        Icons.person,
+                        size: imageSize * 0.6,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  )
+                : Icon(
+                    Icons.person,
+                    size: imageSize * 0.6,
+                    color: Colors.grey[600],
                   ),
-              errorWidget:
-                  (context, url, error) => Container(
-                    color: Colors.grey[300],
-                    child: Icon(
-                      Icons.person,
-                      size: (width ?? SizeUtil.userImageSize(context)) * 0.6,
-                      color: Colors.grey[600],
-                    ),
-                  ),
-            ) : Icon(
-                      Icons.person,
-                      size: (width ?? SizeUtil.userImageSize(context)) * 0.6,
-                      color: Colors.grey[600],
-                    ),
           ),
         ),
       ),
