139a91d956f1f8a42cf5657a5fabc719c6c0a723
Nekoyama141592 <mtaky0904@gmail.com>
2025-08-20 17:20:24 +0900

    remove: flavor

--- lib/presentation/util/drawer_ui_util.dart
+++ lib/presentation/util/drawer_ui_util.dart
@@ -1,7 +1,5 @@
-import 'package:great_talk/application/app/flavors.dart';
 import 'package:great_talk/presentation/constant/remote_config_constants.dart';
 
 class DrawerUiUtil {
-  static String get version =>
-      'ver.${RemoteConfigConstants.appVersion} ${F.appFlavor}';
+  static String get version => 'ver.${RemoteConfigConstants.appVersion}';
 }
